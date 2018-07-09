1fba5fe9a45bedb80c0806eefb47884c33a7e67b
Lior Rabin <lior@colu.com>
2018-07-09 13:38:44 +0300

    implementation and documentation

---
+++ index.js
@@ -0,0 +1,20 @@
+const Osseus = require('osseus')
+
+const init = async (modules, callback) => {
+  try {
+    const osseus = await Osseus.init()
+    const errors = []
+    modules.forEach(module => {
+      if (!osseus[module]) {
+        errors.push(`"${module}" was not initialized, check configurations - https://github.com/colucom/osseus-${module}/blob/master/README.md`)
+      }
+    })
+    errors.length > 0 ? callback(errors) : callback(null, osseus)
+  } catch (err) {
+    callback(err)
+  }
+}
+
+module.exports = {
+  init: init
+}
